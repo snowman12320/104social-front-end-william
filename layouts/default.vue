4d724d02f722d4eb8dc7acc6755de39941c37c0d
snowman12320 <snowman12320@gmail.com>
2024-04-10 18:25:36 +0800

    [新增] 透過npm產生nuxt需要的資料夾

---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="p-10 bg-gray-900 h-screen">
+    Layout: default
+    <slot />
+  </div>
+</template>
---
+++ pages/demo/demo.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    Page: foo
+  </div>
+</template>
